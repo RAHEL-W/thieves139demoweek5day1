32c238743b1f4915d73d05fa984728922b6321ae
RAHEL <richassefa27@gmail.com>
2024-02-10 15:49:43 -0800

    mig

--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,18 @@
-from flask import  request, render_template
+from flask import  request, render_template, redirect, url_for,flash
 import requests
 
 from  app  import app
 from .forms import SignupForm
 from .forms import LoginForm
 from .forms import ErgastForm
+from app.models import User
+from werkzeug.security import check_password_hash
+from flask_login import login_user, logout_user
+
 
 
 @app.route("/")
-def hello_world():
+def home():
     return render_template('home.html')
 
 @app.route('/user/<name>')
@@ -23,9 +27,13 @@ def user(name):
 def signup():
     form = SignupForm()
     if request.method == 'POST' and form.validate_on_submit():
+        username = form.username.data
         email = form.email.data
         password = form.password.data
-        return f'welcome to pokemon thanks for sign up  {email} {password}'
+        new_user =User(username, email, password)
+        new_user.save()
+        flash('success thank  you for sign up', 'success')
+        return redirect(url_for('login'))
     else:
         return render_template('signup.html', form=form)
     
@@ -37,11 +45,23 @@ def login():
     if request.method == 'POST' and form.validate_on_submit():
         email = form.email.data
         password = form.password.data
-        return f'welcome to pokemon {email} {password}'
+
+        queried_user = User.query.filter(User.email == email).first()
+        if queried_user and check_password_hash(queried_user.password, password):
+
+          flash(f'wellcome {queried_user.username}', 'info')
+          login_user(queried_user)
+          return redirect(url_for('home'))
+        else: 
+            flash('incorrect username, email or password....try again',  'warning')
+            return render_template('login.html', form=form)
     else:
         return render_template('login.html', form=form)
         
-    
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))    
 
 
 
